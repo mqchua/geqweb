eaa3297a5dbcf481e1479a04199b093c8dd30d16
Ming <chua_ming_quan@live.com>
2021-03-08 01:53:19 +0800

    added new volunteer groups

--- src/pages/volunteer/VolunteerPage.js
+++ src/pages/volunteer/VolunteerPage.js
@@ -3,9 +3,14 @@ import { Box, Card, CardActions, Typography } from '@material-ui/core';
 import './Volunteer.css';
 import {useStyles} from './styles.js'
 import Grid from '@material-ui/core/Grid';
-import ceb from './images/ceb.jpg';
-import hantublog from './images/hantublog.jpg';
-import seagrass from './images/seagrass.jpg';
+// import ceb from './images/ceb.jpg';
+// import hantublog from './images/hantublog.jpg';
+// import seagrass from './images/seagrass.jpg';
+import climateCon from './images/climateCon.png';
+import ecoSg from './images/ecoSg.png';
+import syca from './images/syca.png';
+import wws from './images/wws.jpeg';
+import zerowastesg from './images/zerowastesg.png';
 
 
 function ArticlesPage(){
@@ -22,6 +27,112 @@ function ArticlesPage(){
             
 
                     <h3 className="eventOverviewHeaders">Volunteer Groups</h3>
+
+                    <hr/>
+
+                    <a href="https://climateconversations.sg/volunteer" target="_blank" rel="noopener noreferrer">
+                        <div class="content">
+
+                            <div className="imgContent" justify="center">
+                                <img className={classes.imgClass} src={climateCon} />
+                            </div>
+
+                            <div class="imgText">
+                                <h5 class="imgTextChild">Climate Conversations </h5>
+                                <h5 class="imgTextChildSub">&rarr;  Provide various opportunities to discuss and exchange ideas with different people.
+                                <br></br>
+                                &rarr;  Offers variety of roles (newsletter editor, video editor, event coordinator etc.)
+                                <br></br>
+                                &rarr;  Deals with various climate issues
+                                </h5>
+                            </div>
+                        </div>
+                    </a>
+
+                    <hr/>
+
+                    <a href="http://www.zerowastesg.com/" target="_blank" rel="noopener noreferrer">
+                        <div class="content">
+
+                            <div className="imgContent" justify="center">
+                                <img className={classes.imgClass} src={ecoSg} />
+                            </div>
+
+                            <div class="imgText">
+                                <h5 class="imgTextChild">ZeroWaste SG </h5>
+                                <h5 class="imgTextChildSub">&rarr;  Devoted to reducing wastes in Singapore (food waste, plastic disposable etc.)</h5>
+                            </div>
+                        </div>
+                    </a>
+
+                    <hr/>
+
+                    <a href="http://www.eco-singapore.org/" target="_blank" rel="noopener noreferrer">
+                        <div class="content">
+
+                            <div className="imgContent" justify="center">
+                                <img className={classes.imgClass} src={syca} />
+                            </div>
+
+                            <div class="imgText">
+                                <h5 class="imgTextChild">ECO Singapore</h5>
+                                <h5 class="imgTextChildSub">&rarr; Challenges and educate the youth ages 17-35 to adopt sustainable lifestyle
+                                     &rarr; Encourage the youth to take initiatives and lead the projects by themselves.
+                                </h5>
+                            </div>
+                        </div>
+                    </a>
+
+                    <hr/>
+
+                    <a href="https://www.wws.org.sg/" target="_blank" rel="noopener noreferrer">
+                        <div class="content">
+
+                            <div className="imgContent" justify="center">
+                                <img className={classes.imgClass} src={wws} />
+                            </div>
+
+                            <div class="imgText">
+                                <h5 class="imgTextChild">Waterways Watch Society</h5>
+                                <h5 class="imgTextChildSub">&rarr;  Devoted to protecting aquatic life, and cleaning waterways and coastlines in SG.</h5>
+                            </div>
+                        </div>
+                    </a>
+
+                    <hr/>
+
+                    <a href="https://syca.sg/" target="_blank" rel="noopener noreferrer">
+                        <div class="content">
+
+                            <div className="imgContent" justify="center">
+                                <img className={classes.imgClass} src={zerowastesg} />
+                            </div>
+
+                            <div class="imgText">
+                                <h5 class="imgTextChild">Singapore Youth for Climate </h5>
+                                <h5 class="imgTextChildSub">&rarr;  Holds educational conferences on the climate change
+                                <br></br>
+                                &rarr;  Holds a number of short-term climate projects annually
+                                </h5>
+                            </div>
+                        </div>
+                    </a>
+
+                    <hr/>
+{/* 
+                    <a href="" target="_blank" rel="noopener noreferrer">
+                        <div class="content">
+
+                            <div className="imgContent" justify="center">
+                                <img className={classes.imgClass} src={ceb} />
+                            </div>
+
+                            <div class="imgText">
+                                <h5 class="imgTextChild"> </h5>
+                                <h5 class="imgTextChildSub"></h5>
+                            </div>
+                        </div>
+                    </a>
 
                     <hr/>
 
@@ -71,7 +182,7 @@ function ArticlesPage(){
                         </div>
                     </a>
 
-                    <hr/>
+                    <hr/> */}
 
 
     
