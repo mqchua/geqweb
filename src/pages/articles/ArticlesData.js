6eec58233733dc209f1925f874c748e0475cd04a
Ming <chua_ming_quan@live.com>
2021-03-08 16:25:19 +0800

    added new file list for articles

---
+++ src/pages/articles/ArticlesData.js
@@ -0,0 +1,58 @@
+// Data export
+export default [
+    {
+        link: 'https://mothership.sg/2021/03/the-breakfast-club-jacinda/',
+        date: 'March 05, 2021',
+        title: 'Singaporean, 32, starts plant-based meal delivery service after Covid-19 disrupts campervan travel to Europe',
+        content: 'A young Singaporean, who had her globe trotting cut short by the Covid-19 pandemic,\
+        returned home to start an organic farm-to-table vegan home kitchen.'
+    },
+    {
+        link: 'https://mothership.sg/2021/03/sentosa-carbon-neutral-2030/',
+        date: 'March 05, 2021',
+        title: 'Sentosa to become carbon-neutral leisure destination by 2030',
+        content: 'Singapore\'s plans to improve sustainability in various sectors will see the resort island of Sentosa transform into a carbon-neutral\
+         leisure destination by 2030. This will be a whole-of-Sentosa effort, involving all businesses on the island,\
+          and a partnership between the Sentosa Development Corporation (SDC) and Temasek.'
+    },
+    {
+        link: 'https://www.straitstimes.com/multimedia/the-big-story-no-more-new-diesel-cars-taxis-in-spore-from-2025',
+        date: 'March 04, 2021',
+        title: 'No more new diesel cars, taxis in S\'pore from 2025',
+        content: 'Opening the debate on the Government\'s sustainability plans in Parliament on Thursday (March 4), Senior \
+        Minister Teo Chee Hean said climate change poses an existential challenge for Singapore. \
+        The country\'s unique constraints also mean that the trade-offs it faces in cutting its emissions are much starker than other countries.'
+    },
+    {
+        date: '',
+        title: '',
+        content: ''
+    },
+    {
+        date: '',
+        title: '',
+        content: ''
+    },
+    {
+        date: '',
+        title: '',
+        content: ''
+    },
+    {
+        date: '',
+        title: '',
+        content: ''
+    },
+    {
+        date: '',
+        title: '',
+        content: ''
+    },
+    {
+        date: '',
+        title: '',
+        content: ''
+    },
+    
+];
+  
